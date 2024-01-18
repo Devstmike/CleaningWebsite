438d9f7150202ed738f6ee823b2057b5e508a5ea
Devstmike <139519652+Devstmike@users.noreply.github.com>
2024-01-18 00:14:50 +0000

    Add files via upload

--- Cleaning.js
+++ Cleaning.js
@@ -1,2 +1,20 @@
-let a = ("https://drive.google.com/file/d/131_jLTCzl4eCnuOrP9bP4SPNftFuzlAu/view?usp=drive_link").split("/");
-console.log(a)
+let header = document.querySelector("header");
+
+let cancel = document.querySelector(".fa-xmark");
+let menuIcon = document.querySelector(".menuIcon");
+
+
+menuIcon.onclick = () => {
+    header.classList.add("showNav")
+    header.classList.add("headerColor");
+    menuIcon.classList.add("hideMenu");
+   
+}
+
+
+cancel.onclick = () => {
+    header.classList.remove("showNav");
+    header.classList.remove("headerColor");
+    menuIcon.classList.remove("hideMenu");
+   
+}
